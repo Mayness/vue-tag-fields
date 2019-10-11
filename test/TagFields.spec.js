024f4e6997824709a851c0561d5653c54f254415
mayness <1095346833@qq.com>
2019-10-11 18:59:08 +0800

    feat: 增加单元测试

--- test/TagFields.spec.js
+++ test/TagFields.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import TagFields from "../src/lib/TagFields.vue";
 
 async function addTag(wrapper, newTag) {
@@ -7,7 +7,7 @@ async function addTag(wrapper, newTag) {
 describe("TagFields.vue", () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = shallowMount(TagFields, {
+    wrapper = mount(TagFields, {
       propsData: {
         value: [],
       }
